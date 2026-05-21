9b0a87bf540d09dc4dbba5b88bbc276700943d73
kay <ora_kay@163.com>
2026-05-21 14:46:03 +0800

    feat: expand schema and diff models from placeholders

--- diff/model.go
+++ diff/model.go
@@ -1,3 +1,31 @@
 package diff
 
-type Result struct{}
+import "dbgold/schema"
+
+type Result struct {
+	AddedTables    []schema.Table
+	DroppedTables  []schema.Table
+	ModifiedTables []TableDiff
+}
+
+type TableDiff struct {
+	TableName          string
+	AddedColumns       []schema.Column
+	DroppedColumns     []schema.Column
+	ModifiedColumns    []ColumnDiff
+	AddedIndexes       []schema.Index
+	DroppedIndexes     []schema.Index
+	AddedConstraints   []schema.Constraint
+	DroppedConstraints []schema.Constraint
+	AddedForeignKeys   []schema.ForeignKey
+	DroppedForeignKeys []schema.ForeignKey
+}
+
+type ColumnDiff struct {
+	Column               schema.Column
+	OldColumn            schema.Column
+	TypeChanged          bool
+	NullableChanged      bool
+	DefaultChanged       bool
+	AutoIncrementChanged bool
+}
--- schema/model.go
+++ schema/model.go
@@ -1,5 +1,83 @@
 package schema
 
-type Schema struct{}
-type FullSchema struct{}
-type SelectedObjects struct{}
+type Schema struct {
+	Name   string
+	Tables []Table
+}
+
+type Table struct {
+	Name        string
+	Columns     []Column
+	Indexes     []Index
+	Constraints []Constraint
+	ForeignKeys []ForeignKey
+}
+
+type Column struct {
+	Name          string
+	Type          string
+	Nullable      bool
+	Default       *string
+	PrimaryKey    bool
+	AutoIncrement bool
+	Comment       string
+}
+
+type Index struct {
+	Name    string
+	Columns []string
+	Unique  bool
+}
+
+type Constraint struct {
+	Name string
+	Type string // CHECK | UNIQUE | PRIMARY
+	Def  string
+}
+
+type ForeignKey struct {
+	Name       string
+	Columns    []string
+	RefTable   string
+	RefColumns []string
+	OnDelete   string
+	OnUpdate   string
+}
+
+type FullSchema struct {
+	Schema
+	Views     []View
+	Sequences []Sequence
+	Triggers  []Trigger
+}
+
+type View struct {
+	Name string
+	Def  string
+}
+
+type Sequence struct {
+	Name      string
+	Start     int64
+	Increment int64
+	MinValue  int64
+	MaxValue  int64
+}
+
+type Trigger struct {
+	Name   string
+	Table  string
+	Event  string
+	Timing string
+	Body   string
+}
+
+type SelectedObjects struct {
+	Tables      []Table
+	Views       []View
+	Sequences   []Sequence
+	Triggers    []Trigger
+	Indexes     []Index
+	Constraints []Constraint
+	ForeignKeys []ForeignKey
+}
